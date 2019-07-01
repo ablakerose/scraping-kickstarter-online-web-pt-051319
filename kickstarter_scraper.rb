57011f51d10af52fd8649840458fb0aff441dc08
Blake Rose <bdecktor@gmail.com>
2019-07-01 17:38:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
 
   #Iterate through the projects
 
-  kickstarter.css("li.project.grid_4").each do |projects| 
+  kickstarter.css("li.project.grid_4").each do |projects|
     projects[project] = {}
   end
 
