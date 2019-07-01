1e4991a18f3fb1b910171f58568f40872f898052
Blake Rose <bdecktor@gmail.com>
2019-07-01 17:35:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,3 +1,5 @@
+# file: kickstarter_scraper.rb
+
 require 'nokogiri'
 require 'pry'
 # require libraries/modules here
@@ -12,3 +14,7 @@ create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
