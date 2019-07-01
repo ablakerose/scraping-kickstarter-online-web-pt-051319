8f70f734c91577449464430ffaa9eba506e44b74
Blake Rose <bdecktor@gmail.com>
2019-07-01 17:37:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 end
-# projects: kickstarter.css("li.project.grid_4")
-create_project_hash
+
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
