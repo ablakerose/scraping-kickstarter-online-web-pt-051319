2900b4df0f002edf8e7df3dccc4033ddbb657370
Blake Rose <bdecktor@gmail.com>
2019-07-01 17:38:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,15 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #Iterate through the projects
+
+  kickstarter.css("li.project.grid_4").each do |projects| 
+    projects[project] = {}
+  end
+
 end
 
 
